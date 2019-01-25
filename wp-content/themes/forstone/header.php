319900f074cc1cef069bd822119301abd7a2ad2f
Bryan Vidal <bryan.vidal@cifacom.com>
2019-01-25 23:26:25 +0100

    recette suite

--- wp-content/themes/forstone/header.php
+++ wp-content/themes/forstone/header.php
@@ -11,6 +11,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <?php wp_head(); ?>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133244680-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-133244680-1');
+    </script>
+
+
   </head>
 
   <body <?php body_class(); ?>>
